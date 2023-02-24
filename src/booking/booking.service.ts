26f0f8b014590974043c7159a7c00921409921df
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-02-24 12:33:34 -0600

    Add booking service business logic

---
+++ src/booking/booking.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import {BookingDTO} from "./dto/create-booking.dto";
+import {v4} from 'uuid';
+
+@Injectable()
+export class BookingService {
+
+    bookings: BookingDTO[] = [];
+
+    create(booking: BookingDTO) {
+        console.log(BookingDTO);
+
+        booking.bookingId = v4();
+
+        this.bookings = [...this.bookings, booking];
+
+        return this.bookings;
+    }
+
+    getAll() {
+        return this.bookings;
+    }
+}
